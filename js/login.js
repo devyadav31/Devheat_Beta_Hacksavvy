435c3d10e69b51f7b9177d19024df0922fe83fda
devyadav31 <31yadav.dev@gmail.com>
2023-09-30 23:56:45 +0530

    Js for login page

---
+++ js/login.js
@@ -0,0 +1,11 @@
+function closeForm() {
+    // Redirect to the home page URL after closing.
+    window.location.href = 'cosmo capital.html';
+}
+
+// You can add JavaScript logic here for handling the login functionality if needed.
+document.getElementById('login-button').addEventListener('click', function() {
+    // Add your login logic here.
+    // For a simple example, you can alert a message.
+    alert('Login button clicked');
+});
