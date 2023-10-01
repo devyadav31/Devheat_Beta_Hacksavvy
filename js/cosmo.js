e840eb633b6f911f7e9d49302b18b76d47845caa
devyadav31 <31yadav.dev@gmail.com>
2023-10-01 21:14:49 +0530

    animation added in home page of cosmoeco capital

---
+++ js/cosmo.js
@@ -0,0 +1,91 @@
+const ctx = document.getElementById('stockChart').getContext('2d');
+const stockChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [
+            {
+                label: 'Stock Price 1',
+                borderColor: 'rgb(54, 162, 235)',
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderWidth: 2,
+                pointRadius: 2,
+                pointBackgroundColor: 'rgba(54, 162, 235, 1)',
+                pointBorderColor: 'rgba(54, 162, 235, 1)',
+                data: [],
+                fill: true
+            },
+            {
+                label: 'Stock Price 2',
+                borderColor: 'rgb(255, 99, 132)', // Different color
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderWidth: 2,
+                pointRadius: 2,
+                pointBackgroundColor: 'rgba(255, 99, 132, 1)',
+                pointBorderColor: 'rgba(255, 99, 132, 1)',
+                data: [],
+                fill: true
+            }
+        ]
+    },
+    options: {
+        responsive: false,
+        width: 600,
+        height: 300,
+        animation: {
+            duration: 200, // Add a subtle animation
+        },
+        scales: {
+            x: [{
+                type: 'linear',
+                position: 'bottom',
+                ticks: {
+                    max: 10,
+                    stepSize: 1,
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Seconds'
+                },
+                gridLines: {
+                    display: false,
+                }
+            }],
+            y: [{
+                ticks: {
+                    min: 800,
+                    max: 900,
+                    stepSize: 20,
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Price (INR)'
+                },
+                gridLines: {
+                    color: 'rgba(0, 0, 0, 0.1)',
+                }
+            }]
+        }
+    }
+});
+
+let elapsedTime = 0;
+
+function updateChart() {
+    const labels = Array.from({ length: 10 }, (_, index) => elapsedTime + index);
+    
+    // Simulated stock prices for dataset 1 within the range of 800 to 900
+    const stockValues1 = labels.map(() => 800 + Math.random() * 100);
+    
+    // Simulated stock prices for dataset 2 within the range of 800 to 900
+    const stockValues2 = labels.map(() => 800 + Math.random() * 100);
+
+    stockChart.data.labels = labels;
+    stockChart.data.datasets[0].data = stockValues1;
+    stockChart.data.datasets[1].data = stockValues2;
+
+    stockChart.update();
+    elapsedTime += 1; // Increase elapsed time by 1 second
+}
+
+setInterval(updateChart, 2000);  
